001a63b85e4b435258c84b73ca6b363444d77223
Vanessa Me Tonini <vanessametonini@gmail.com>
2017-04-08 13:28:12 -0300

    Shapely Child Theme

---
+++ shapely-child/functions.php
@@ -0,0 +1,35 @@
+<?php
+// add_action( 'wp_enqueue_scripts', 'theme_enqueue_styles' );
+// function theme_enqueue_styles() {
+//     wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' );
+
+// }
+
+
+function theme_enqueue_styles() {
+
+    $parent_style = 'parent-style';
+
+    wp_enqueue_style( $parent_style, get_template_directory_uri() . '/style.css' );
+    wp_enqueue_style( 'child-style',
+        get_stylesheet_directory_uri() . '/style.css',
+        array( $parent_style )
+    );
+}
+add_action( 'wp_enqueue_scripts', 'theme_enqueue_styles' );
+
+
+function logo_size_change(){
+	remove_theme_support( 'custom-logo' );
+	add_theme_support( 'custom-logo', array(
+	    'height'      => 100,
+	    'width'       => 400,
+	    'flex-height' => true,
+	    'flex-width'  => true,
+	) );
+}
+
+add_action( 'after_setup_theme', 'logo_size_change', 11 );
+
+
+?>
